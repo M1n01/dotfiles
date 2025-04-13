e58c98621a8c223183dacc3e3d7b6622bf1b10f9
M1n01 <abe.minato.bz@gmail.com>
2025-04-13 10:52:20 +0900

    update: change VSCode extension saving script to use code-insiders and update extensions list

--- .bin/save_code_extentions.sh
+++ .bin/save_code_extentions.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Saving VSCode extensions..."
-code --list-extensions > "$HOME"/dotfiles/vscode/code_extensions
+code-insiders --list-extensions > "$HOME"/dotfiles/vscode/code_extensions
